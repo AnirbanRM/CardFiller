9e120e7530b28792f6e6173c7fd19341246367be
Anirban <anirbanroymukherjee@outlook.com>
2021-02-21 09:58:06 +0530

    v1.0 complete

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
 import javafx.scene.canvas.*;
@@ -11,9 +13,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -61,7 +66,40 @@ public class Controller implements Initializable {
     @FXML
     AnchorPane mainpane;
 
+    Element activeElement;
+
+    private double distance(double X1, double Y1, double X2, double Y2){
+        return Math.sqrt(Math.pow((X1-X2),2) + Math.pow((Y1-Y2),2));
+    }
+
     public void  makeDraggable(){
+
+        canvas.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                for(Element e : droppedEntities){
+                    if(distance(e.X,e.Y,mouseEvent.getX(),mouseEvent.getY())<=5) {
+                        activeElement = e;
+                        break;
+                    }
+                }
+            }
+        });
+
+        canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(activeElement!=null){
+                    activeElement.X = mouseEvent.getX();
+                    activeElement.Y = mouseEvent.getY();
+                    try {
+                        redraw();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
         canvas.setOnDragOver(new EventHandler<DragEvent>() {
             @Override
@@ -69,6 +107,13 @@ public class Controller implements Initializable {
                 if(dragEvent.getDragboard().hasString()){
                     dragEvent.acceptTransferModes(TransferMode.COPY);
                 }
+            }
+        });
+
+        canvas.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                activeElement = null;
             }
         });
 
@@ -83,7 +128,7 @@ public class Controller implements Initializable {
 
                 try {
                     redraw();
-                }catch (Exception e){}
+                }catch (Exception e){e.printStackTrace();}
             }
         });
 
@@ -141,7 +186,7 @@ public class Controller implements Initializable {
             }
             fis.close();
             populatePlaceholderList(t);
-        }catch (Exception e){}
+        }catch (Exception e){e.printStackTrace();}
     }
 
     private void load_image(File f){
@@ -153,6 +198,7 @@ public class Controller implements Initializable {
         try {
             card_img = ImageIO.read(f);
         }catch (Exception e){
+            e.printStackTrace();
             return;
         }
 
@@ -160,7 +206,9 @@ public class Controller implements Initializable {
         canvas.setHeight(card_img.getHeight());
         try {
             redraw();
-        }catch (Exception e){}
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private void redraw() throws Exception{
@@ -206,8 +254,83 @@ public class Controller implements Initializable {
                 load_csv(fileChooser.showOpenDialog(curr_stg));
             }
         });
+
+        output_browse.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                DirectoryChooser dirChooser = new DirectoryChooser();
+                File f = dirChooser.showDialog(curr_stg);
+
+                if(f==null)
+                    return;
+
+                output_dir_box.setText(f.getPath());
+            }
+        });
+
+        export_button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                export();
+            }
+        });
         droppedEntities = new ArrayList<Element>();
 
         makeDraggable();
     }
+
+    private void export(){
+        try {
+            FileInputStream fis = new FileInputStream(csv_file_path);
+            BufferedReader bfr = new BufferedReader(new InputStreamReader(fis));
+
+            boolean head = true;
+
+            int i = 1;
+
+            while(true){
+                String s = bfr.readLine();
+
+                if(s==null)
+                    break;
+
+                else if(s.length()!=0 && head)
+                    head = false;
+
+                else{
+                    String[] d = s.split(",");
+                    export_a_record(d,i++);
+                }
+            }
+
+            //fis.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void export_a_record(String[] a,int num){
+
+        Canvas c = new Canvas(card_img.getWidth(),card_img.getHeight());
+        GraphicsContext g = c.getGraphicsContext2D();
+
+        g.drawImage(card_img_prepared,0,0);
+
+        for(Element i : droppedEntities){
+            try {
+                g.fillText(a[placeHolders.get(i.entity)], i.X, i.Y);
+            }catch (Exception e){}
+        }
+
+        WritableImage wi = new WritableImage((int)c.getWidth(),(int)c.getHeight());
+        SnapshotParameters sp = new SnapshotParameters();
+        sp.setFill(Color.TRANSPARENT);
+
+        File img_file = new File(output_dir_box.getText()+"\\"+num+".png");
+        try {
+            img_file.createNewFile();
+            ImageIO.write(SwingFXUtils.fromFXImage(c.snapshot(sp,wi), null), "png", img_file);
+        }catch (Exception e){}
+    }
+
 }
